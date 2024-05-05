39eb546488745f5af71b308b18091b53c0260815
maboglia <mauro.bogliaccino@gmail.com>
2024-05-05 14:18:38 +0200

    agg

---
+++ esempi/spring-boot-angular/src/main/java/com/maboglia/application/Application.java
@@ -0,0 +1,28 @@
+package com.maboglia.application;
+
+import com.maboglia.application.entities.User;
+import com.maboglia.application.repositories.UserRepository;
+import java.util.stream.Stream;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("Newton", "Galilei", "Curie", "Pascal", "Tesla").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@gmail.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ esempi/spring-boot-angular/src/main/java/com/maboglia/application/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.maboglia.application.repositories;
+
+import com.maboglia.application.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{}
